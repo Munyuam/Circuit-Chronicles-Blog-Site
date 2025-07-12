fe8c7736d6ce0c6c36c9ac1fbc7acd1e506fecd9
mwiza <munyumasichinga66@gmail.com>
2025-07-12 14:53:04 +0200

    4th commit
    added a layout folder and main ejs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
-// const mongoose = require('mongoose');
 const route = require('./routes/route')
+const expressLayouts = require('express-ejs-layouts')
 const path = require('path');
 const port = process.env.PORT || 3001;
 const app = express();
@@ -11,12 +11,20 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layout/main');
+app.use(expressLayouts);
+
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/', route);
 
 app.use((req,res)=>{
     res.status(404).render('404');
+})
+
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).render('500', {err: err.message});
 })
 
 app.listen(port, ()=>{
